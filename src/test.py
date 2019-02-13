289a7f943c58c5b615aa7d564747fe1d49dd121b
Justin Baum <justin.baum@ninefx.com>
2019-02-13 15:28:19 -0500

    Break out some tests

--- src/test.py
+++ src/test.py
@@ -13,3 +13,18 @@ tx = Transaction()
 wallet.signTX(tx, -1)
 print("Wallet signatures: ")
 print(tx.signatures)
+
+print("The new TX hash is: ")
+print(tx.get_unsigned_hash())
+
+block = Block()
+
+print("We will make a block: " + str(block.get_hash()))
+block.tx.append(tx)
+
+print("We will add the tx: " + str(block.get_hash()))
+print("We will now mine it until the first 4 hex digits are 0")
+block.difficulty = 16
+block.mine()
+print("The block finished mining at nonce: " + str(block.nonce))
+print("The new hash is: " + str(block.get_hash()))
