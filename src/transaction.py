27de881dd587955391c61e293aeaa055414a94a7
Justin Baum <justin.baum@ninefx.com>
2019-02-13 15:16:01 -0500

    Patch; Signatures work (#5)
    * Transactions fixed
    
    * Add patch, and fix other stuff
    
    * Add new scripts to readme

--- src/transaction.py
+++ src/transaction.py
@@ -8,31 +8,37 @@ from tools import tobytes, getbytes
 
 class Transaction:
   def __init__(self, origins = [], destinations = []):
-    #  origins are tuples of (tx hashes, and addresses)
-    #  destinations are tuples of (address, and amounts)
+    # origins are tuples of (tx hashes, and addresses)
+    # destinations are tuples of (address, and amounts)
     self.origins = origins
     self.destinations = destinations
+    # Signatures are (public x, public y, r, q)
     self.signatures = []
     self.hash = None
-  def serialize_unhashed(self):
-    #  start a byte sequence
+  def serialize_unsigned(self):
+    # start a byte sequence
     serial = tobytes(0,0)
     serial += tobytes(len(self.origins), 1)
     serial += tobytes(len(self.destinations), 1)
     for origin in self.origins:
-      #  64 bytes for each input
-      serial += tobytes(origin[0], 32)
-      serial += tobytes(origin[1], 32)
+      # 64 bytes for each input
+      serial += tobytes(origin[0], 32)  # tx hash of origin
+      serial += tobytes(origin[1], 32)  # public x of origin
     for destination in self.destinations:
-      #  16 bytes for each output
-      serial += tobytes(destination[0], 32)
-      serial += tobytes(destination[1], 32)
+      # 64 bytes for each output
+      serial += tobytes(destination[0], 32)  # public x of destination
+      serial += tobytes(destination[1], 32)  # amount in units
     return serial
   def serialize(self):
-    serial = self.serialize_unhashed()
+    serial = self.serialize_unsigned()
+    # 1 bytes for num of signatures
+    serial += tobytes(len(self.signatures),1)
     for signature in self.signatures:
-      serial += tobytes(signature[0], 32)
-      serial += tobytes(signature[1], 32)
+      serial += tobytes(signature[0], 32)  # Public x
+      serial += tobytes(signature[1], 32)  # Public y
+      # Signatures part
+      serial += tobytes(signature[2], 32)  # r
+      serial += tobytes(signature[3], 32)  # q
     return serial
   def from_serial(self, message):
     (nInputs, message) = getbytes(1, message)
@@ -45,4 +51,16 @@ class Transaction:
       (address, message) = getbytes(32, message)
       (value, message) = getbytes(32, message)
       self.destinations.append((address, value))
+    (nSignatures, message) = getbytes(1, message)
+    for i in range(nSignatures):
+      (px, message) = getbytes(4, message)
+      (py, message) = getbytes(4, message)
+      (r, message)  = getbytes(4, message)
+      (q, message)  = getbytes(4, message)
+      self.signatures((px, py, r, q))
     # Signatures
+  def get_unsigned_hash(self):
+    m = self.serialize_unsigned()
+    hash = sha256()
+    hash.update(m)
+    
