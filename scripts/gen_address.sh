b32743a4c4c24dd71a61ba46b2de76f6256d5393
Justin Baum <justinbaum5@gmail.com>
2021-11-25 02:06:11 -0500

    Little glitch with string

--- scripts/gen_address.sh
+++ scripts/gen_address.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
 pushd $SCRIPT_DIR/../src/
-  python3 -c 'from keys import Keys; keys = Keys(); keys.genkey(); keys.saveToFile(); print("Generated: "+keys.getaddresses()[-1])'
+python3 -c 'from keys import Keys; keys = Keys(); keys.genkey(); keys.saveToFile(); print("Generated: "+str(keys.getaddresses()[-1]))'
 popd
